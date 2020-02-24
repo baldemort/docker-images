3be2718deda8f31347f6b7d0d63fc39fb5ec7a3e
Hang Xie <7977860+hangxie@users.noreply.github.com>
2020-02-24 13:25:49 -0800

    run postfix in background

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -3,14 +3,16 @@
 set -euo pipefail
 
 # Prepare for running environment
-mkdir -p /run/courier/authdaemon /var/spool/postfix/public /var/spool/postfix/maildrop
+mkdir -p \
+    /run/courier/authdaemon \
+    /var/spool/postfix/public \
+    /var/spool/postfix/maildrop
 chgrp postdrop /var/spool/postfix/public /var/spool/postfix/maildrop
-
 
 # Launch courier daemon and postfix
 /usr/lib/courier/courier-authlib/authdaemond 2>&1 \
 | /usr/bin/ts '%b %d %H:%M:%S mail courier/authdaemon' &
-postfix start-fg 2>&1
+postfix start-fg 2>&1 &
 
 # Exit container in case either one stops
 wait -n
