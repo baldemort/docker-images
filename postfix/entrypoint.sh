7bbcc1b2e4962c2d81cee2bc75d81052b20bb9d8
Hang Xie <7977860+hangxie@users.noreply.github.com>
2020-02-25 14:54:13 -0800

    fix postfix conf location

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -14,7 +14,7 @@ chown postfix:postdrop \
 # Launch courier daemon and postfix
 /usr/lib/courier/courier-authlib/authdaemond 2>&1 \
 | /usr/bin/ts '%b %d %H:%M:%S mail courier/authdaemon' &
-postfix start-fg 2>&1 &
+postfix -c /etc/postfix start-fg 2>&1 &
 
 # Exit container in case either one stops
 wait -n
